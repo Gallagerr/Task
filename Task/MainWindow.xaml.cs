ce56359835c1085d68f4b669ab268e1963da3ca3
sadiev <sadiev@itstep.kz>
2019-06-13 21:40:21 +0600

    asd

--- Task/MainWindow.xaml.cs
+++ Task/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using System.Threading;
+using System.IO;
 
 namespace Task
 {
@@ -32,22 +34,56 @@ namespace Task
     }
     void timer_Tick(object sender, EventArgs e)
     {
-      lblTime.Content = DateTime.Now.ToLongTimeString();
+      lblTimeDownload.Content = DateTime.Now.ToLongTimeString();
+      lblTimeCatalog.Content = DateTime.Now.ToLongTimeString();
+      lblTimeEmail.Content = DateTime.Now.ToLongTimeString();
     }
 
-    private void Save_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+    private void Save_Click(object sender, RoutedEventArgs e)
     {
-
+      SaveGrid.Visibility = Visibility.Visible;
     }
 
-    private void MovieCatalog_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+    private void MovieCatalog_Click(object sender, RoutedEventArgs e)
     {
+      CatalogGrid.Visibility = Visibility.Visible;
 
+      //FromCatalog sourceDirectory;
+      //ToCatalog targetDirectory;
+    //  Copy(sourceDirectory, targetDirectory);
     }
 
-    private void SendMail_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+    private void SendMail_Click(object sender, RoutedEventArgs e)
     {
-
+      EmailGrid.Visibility = Visibility.Visible;
     }
+
+    public static void Copy(string sourceDirectory, string targetDirectory)
+    {
+      DirectoryInfo diSource = new DirectoryInfo(sourceDirectory);
+      DirectoryInfo diTarget = new DirectoryInfo(targetDirectory);
+
+      CopyAll(diSource, diTarget);
+    }
+    public static void CopyAll(DirectoryInfo source, DirectoryInfo target)
+    {
+      // Если директория target.FullName не существует, создать ее
+      if (!Directory.Exists(target.FullName))
+      {
+        Directory.CreateDirectory(target.FullName);
+      }
+      // Копируем файлы из sourceDirectory в targetDirectory
+      foreach (FileInfo fi in source.GetFiles())
+      {
+        fi.CopyTo(System.IO.Path.Combine(target.ToString(), fi.Name), true);
+      }
+      // Копируем поддиректории
+      foreach (DirectoryInfo diSourceSubDir in source.GetDirectories())
+      {
+        DirectoryInfo nextTargetSubDir = target.CreateSubdirectory(diSourceSubDir.Name);
+        CopyAll(diSourceSubDir, nextTargetSubDir);
+      }
+    }
+
   }
 }
